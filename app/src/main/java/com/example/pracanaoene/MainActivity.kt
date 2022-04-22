9459aeff0febd89f0615698febd28f730fc439e9
student <mlodymaks@gmail.com>
2022-04-22 14:50:01 +0200

    commit

--- app/src/main/java/com/example/pracanaoene/MainActivity.kt
+++ app/src/main/java/com/example/pracanaoene/MainActivity.kt
@@ -1,27 +1,47 @@
 package com.example.pracanaoene
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import android.widget.CalendarView
+import android.text.format.DateFormat
+import android.widget.Button
 import android.widget.DatePicker
-import android.widget.Toast
-import java.text.SimpleDateFormat
+import android.widget.TextView
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
+    lateinit var textView: TextView
+    lateinit var button: Button
+    var day = 0
+    var month: Int = 0
+    var year: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val datePicker = findViewById<DatePicker>(R.id.date_Picker)
-        val today = Calendar.getInstance()
-        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
-            today.get(Calendar.DAY_OF_MONTH)
-
-        ) { view, year, month, day ->
-            val month = month + 1
-            val msg = "You Selected: $day/$month/$year"
-            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        title = "KotlinApp"
+        textView = findViewById(R.id.textView1)
+        button = findViewById(R.id.btnPick1)
+        button.setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            day = calendar.get(Calendar.DAY_OF_MONTH)
+            month = calendar.get(Calendar.MONTH)
+            year = calendar.get(Calendar.YEAR)
+            val datePickerDialog =
+                DatePickerDialog(this@MainActivity, this@MainActivity, year, month,day)
+            datePickerDialog.show()
         }
     }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        TODO("Not yet implemented")
+    }
+
 
 }
